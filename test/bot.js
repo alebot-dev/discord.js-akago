bdc6368d26ff1a98d5166ef72a661aa8ff843016
Max Hurst <max.hurst@hotmail.com>
2020-11-02 17:27:33 +0000

    test: updated Akago Test Client

--- test/bot.js
+++ test/bot.js
@@ -1,5 +1,3 @@
 const TestClient = require('./struct/TestClient');
 const client = new TestClient();
-
-const { token } = require('./config.json');
-client.start(token);
+client.start();
--- test/struct/TestClient.js
+++ test/struct/TestClient.js
@@ -4,12 +4,13 @@ module.exports = class TestClient extends AkagoClient {
     constructor() {
         super({
             ownerID: ['611466971371929602'],
+            token: require('../config.json').token,
         });
 
         this.on('ready', () => console.log('Yoo this is ready!'));
     }
 
-    start(token) {
-        super.login(token);
+    start() {
+        this.login();
     }
 };
