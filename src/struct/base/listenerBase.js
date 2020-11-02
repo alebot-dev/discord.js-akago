4cd055e3927e3fb11e6204c54715d1b10b210adf
Max Hurst <max.hurst@hotmail.com>
2020-11-02 19:24:33 +0000

    base: added comments to listener base

--- src/struct/base/listenerBase.js
+++ src/struct/base/listenerBase.js
@@ -1,9 +1,25 @@
 module.exports = class Listener {
 
     constructor(client, name, options = {}) {
+        /**
+         * Akago Framework Client
+         * @type {Object}
+         */
         this.client = client;
+        /**
+         * Name of the listener
+         * @type {String}
+         */
         this.name = name;
+        /**
+         * Type of the listener either on or once
+         * @type {String}
+         */
         this.type = options.once ? 'once' : 'on';
+        /**
+         * The listener emitter
+         * @type {String}
+         */
         this.emitter = (typeof options.emitter === 'string' ? this.client[options.emitter] : options.emitter) || this.client;
     }
 
