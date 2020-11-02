3038e3c5de3981e604eb04455f4d9febe87805dd
Max Hurst <max.hurst@hotmail.com>
2020-11-02 19:09:44 +0000

    struct: added listenerDirectory to Akago Client

--- src/struct/AkagoClient.js
+++ src/struct/AkagoClient.js
@@ -1,10 +1,11 @@
 const { Client } = require('discord.js');
+const listenerHandler = require('./listeners/listenerHandler.js');
 
 module.exports = class AkairoClient extends Client {
     constructor(options = {}, clientOptions) {
         super(clientOptions || options);
 
-        const { ownerID = '', token = '', prefix = '!' } = options;
+        const { ownerID = '', token = '', prefix = '!', listenerDirectory = '' } = options;
 
         /**
          * The ID of the owner(s).
@@ -23,9 +24,16 @@ module.exports = class AkairoClient extends Client {
          * @type {String}
          */
         this.prefix = prefix;
+
+        /**
+         * Your file path to your listeners folder
+         * @type {String}
+         */
+        this.listenerDirectory = listenerDirectory;
     }
 
     login() {
         super.login(this.token);
+        listenerHandler(this);
     }
 };
