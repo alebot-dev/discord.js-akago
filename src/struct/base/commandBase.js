51ad6ff49ffab348b75140f3b1f95f52ebb17e16
Max Hurst <max.hurst@hotmail.com>
2020-11-06 16:22:10 +0000

    struct: using tenary operators in command base + removed type error checks in command registry

--- src/struct/base/commandBase.js
+++ src/struct/base/commandBase.js
@@ -10,63 +10,62 @@ module.exports = class Command {
          * The name of the command
          * @type {String}
          */
-        this.name = options.name || name;
+        this.name = typeof options.name === 'string' ? options.name : name;
         /**
          * Breif description of the command
          * @type {String}
          */
-        this.description = options.description || null;
+        this.description = typeof options.description === 'string' ? options.description : null;
         /**
          * Usage of the command
          * @type {String}
          */
-        this.usage = options.usage || null;
+        this.usage = typeof options.usage === 'string' ? options.usage : null;
         /**
          * Collection of aliases for the command
          * @type {Array}
          */
-        this.aliases = options.aliases || [];
+        this.aliases = Array.isArray(options.aliases) ? options.aliases : [];
         /**
          * Examples of the usage of the command
          * @type {String|Array}
          */
-        this.examples = options.examples || null;
+        this.examples = (Array.isArray(options.examples) || typeof options.examples === 'string') ? options.examples : [];
         /**
          * The category for the command
          * @type {String}
          */
-        this.category = options.category || null;
+        this.category = typeof options.category === 'string' ? options.category : null;
         /**
          * The cooldown of a command in seconds
          * @type {Number}
          */
-        this.cooldown = options.cooldown || null;
+        this.cooldown = typeof options.cooldown === 'number' ? options.cooldown : null;
         /**
          * Only allows usr's in the ownerID collection in Akago Client option to use the command
          * @type {Boolean}
          */
-        this.ownerOnly = options.ownerOnly || false;
+        this.ownerOnly = typeof options.ownerOnly === 'boolean' ? options.ownerOnly : false;
         /**
          * Only allowing commands to be executed in guilds not dms
          * @type {Boolean}
-         *
          */
-        this.guildOnly = options.guildOnly === undefined ? true : options.guildOnly;
+        this.guildOnly = typeof options.guildOnly === 'boolean' ? (options.guildOnly === undefined ? true : options.guildOnly) : true;
         /**
          * Won't send a message if true and the message channel isn't NSFW
          * @type {Boolean}
          */
-        this.nsfw = options.nsfw || false;
+        this.nsfw = typeof options.nsfw === 'boolean' ? options.nsfw : false;
         /**
          * Collection of permissions the member requires
          * @type {Array}
          */
-        this.memberPermissions = options.memberPermissions || [];
+        this.memberPermissions = Array.isArray(options.memberPermissions) ? options.memberPermissions : [];
         /**
          * Collection of permissions the client requires
          * @type {Array}
          */
-        this.clientPermissions = options.clientPermissions || [];
+        this.clientPermissions = Array.isArray(options.clientPermissions) ? options.clientPermissions : [];
     }
 
 }; 
--- src/struct/registries/commandRegistry.js
+++ src/struct/registries/commandRegistry.js
@@ -11,18 +11,6 @@ module.exports = (client) => {
             if (!client.util.isClass(File)) throw new TypeError(`Akago: Command '${name}' doesn't export a class.`);
             const command = new File(client, name.toLowerCase());
             if (!(command instanceof CommandBase)) throw new TypeError(`Akago: Command '${command.name}' name dosn't extend the command base.`);
-            if (command.name && typeof command.name !== 'string') throw new TypeError(`Akago: Command '${command.name}' description needs to be a string.`);
-            if (command.description && typeof command.description !== 'string') throw new TypeError(`Akago: Command '${command.name}' needs to be a string.`);
-            if (command.usage && typeof command.usage !== 'string') throw new TypeError(`Akago: Command '${command.name}' usage needs to be a string.`);
-            if (command.aliases && !Array.isArray(command.aliases)) throw new TypeError(`Akago: Command '${command.name}' aliases need to be an array.`);
-            if (command.examples && (!Array.isArray(command.examples) && typeof command.examples !== 'string')) throw new TypeError(`Akago: Command '${command.name}' examples needs to be an array or string`);
-            if (command.category && typeof command.category !== 'string') throw new TypeError(`Akago: Command '${command.name}' category needs to be a string.`);
-            if (command.cooldown && typeof command.cooldown !== 'number') throw new TypeError(`Akago: Command '${command.name}' cooldown needs to be a number.`);
-            if (command.ownerOnly && typeof command.ownerOnly !== 'boolean') throw new TypeError(`Akago: Command '${command.name}' ownerOnly needs to be a boolean.`);
-            if (command.guildOnly && typeof command.guildOnly !== 'boolean') throw new TypeError(`Akago: Command '${command.name}' guildOnly needs to be a boolean.`);
-            if (command.nsfw && typeof command.nsfw !== 'boolean') throw new TypeError(`Akago: Command '${command.name}' nsfw needs to be a boolean.`);
-            if (command.memberPermissions && !Array.isArray(command.memberPermissions)) throw new TypeError(`Akago: Command '${command.name}' memberPermissions needs to be an array.`);           
-            if (command.clientPermissions && !Array.isArray(command.clientPermissions)) throw new TypeError(`Akago: Command '${command.name}' clientPermissions needs to be an array.`);
             client.commands.set(command.name, command);
             if (command.aliases.length) {
                 for (const alias of command.aliases) {
