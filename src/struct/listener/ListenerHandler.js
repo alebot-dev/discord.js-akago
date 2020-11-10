d0ecedd7cee49ed194d9d73e22e8948f2ef3e03f
Max Hurst <max.hurst@hotmail.com>
2020-11-10 19:34:50 +0000

    listenerHandler: reload listener function

--- src/struct/listener/ListenerHandler.js
+++ src/struct/listener/ListenerHandler.js
@@ -43,10 +43,20 @@ class ListenerHandler {
         const listener = new File(this.client, name.toLowerCase());
         if (!(listener instanceof ListenerBase)) throw new TypeError(`Akago: Listener '${name}' dosn't extend the listener base.`);
         const emitter = (typeof listener.emitter === 'string' ? this.client[listener.emitter] : listener.emitter) || this.client;
-        this.client.events.set(listener.name, listener);
         listener.client = this.client;
         listener.filepath = filepath;
+        this.client.events.set(listener.name, listener);
         emitter[listener.type]((listener.name || name), (...args) => listener.execute(...args));
+    }
+
+    reloadListener(name) {
+        const listener = this.client.events.get(name);
+        if (!listener) throw new Error(`Akago: listenerHandler reloadListener ${name} isn't a event.`);
+        delete require.cache[require.resolve(listener.filepath)];
+        this.client.events.delete(listener.name);
+        const emitter = (typeof listener.emitter === 'string' ? this.client[listener.emitter] : listener.emitter) || this.client;
+        emitter.removeListener(listener.eventName, listener.execute);
+        this.loadListener(listener.filepath);
     }
 
 }
