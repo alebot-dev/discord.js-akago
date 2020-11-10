47ad7f5bb438e6e906d2372a7f6039331e208d09
Max Hurst <max.hurst@hotmail.com>
2020-11-10 18:54:28 +0000

    listenerHandler: setting the listener to the clients events collection when loaded

--- src/struct/listener/ListenerHandler.js
+++ src/struct/listener/ListenerHandler.js
@@ -43,6 +43,7 @@ class ListenerHandler {
         const listener = new File(this.client, name.toLowerCase());
         if (!(listener instanceof ListenerBase)) throw new TypeError(`Akago: Listener '${name}' dosn't extend the listener base.`);
         const emitter = (typeof listener.emitter === 'string' ? this.client[listener.emitter] : listener.emitter) || this.client;
+        this.client.events.set(listener.name, listener);
         listener.client = this.client;
         listener.filepath = filepath;
         emitter[listener.type]((listener.name || name), (...args) => listener.execute(...args));
