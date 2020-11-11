2b8553d01919f3be85dfbcdcb529261e1fcc77f4
Max Hurst <max.hurst@hotmail.com>
2020-11-11 09:29:00 +0000

    Constants: added MISSING_CLIENT_PERMISSIONS to constants

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,6 +1,7 @@
 module.exports = {
     CommandHandlerEvents: {
         MISSING_MEMBER_PERMISSIONS: 'missingMemberPermissions',
+        MISSING_CLIENT_PERMISSIONS: 'missingClientPermissions',
         COOLDOWN: 'cooldown',
     },
 };
