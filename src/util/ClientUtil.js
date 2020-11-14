b57d2370c42193566856f510fce418f0fd1c7194
iColtz <max.hurst@hotmail.com>
2020-11-14 05:09:46 +0000

    Util: removed isClass from ClientUtil

--- src/util/ClientUtil.js
+++ src/util/ClientUtil.js
@@ -17,17 +17,6 @@ class ClientUtil {
     }
 
     /**
-     * Checks if a function exports a class.
-     * @param {Function} - Function to check if it exports a class.
-     * @returns {boolean}
-     */
-    isClass(given) {
-        return typeof given === 'function' &&
-            typeof given.prototype === 'object' &&
-            new String(given).substring(0, 5) === 'class';
-    }
-
-    /**
      * Capitalizes the first character of a string.
      * @param {string} - String to be capitalized
      * @returns {string}
