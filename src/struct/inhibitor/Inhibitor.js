5759b6fa1d7f54ff79c88f28dcba609e0ed85c3d
iColtz <max.hurst@hotmail.com>
2020-11-14 07:05:21 +0000

    Inhibitor: added comments to inhibitor

--- src/struct/inhibitor/Inhibitor.js
+++ src/struct/inhibitor/Inhibitor.js
@@ -1,7 +1,19 @@
 class Inhibitor {
+    /**
+     * Creates a new command.
+     * @param {string} name - Name of the inhibitor.
+     * @param {opts} [opts={}] - Custom options for the inhibitor class.
+     */
     constructor(name, opts = {}) {
+        /**
+         * The name of the inhibitor
+         * @type {string}
+         */
         this.name = typeof name === 'string' ? name : '';
-
+        /**
+         * Custom options for the inhibitor
+         * @type {object}
+         */
         if (opts && typeof opts === 'object') this.opts = opts;
     }
 }
